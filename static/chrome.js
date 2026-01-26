8bb9d9f1623df4e93ea066afd060221bdf1fa026
githaigakelvin <githaigakelvin9798@gmail.com>
2026-01-26 13:51:20 +0300

    html basics

---
+++ static/chrome.js
@@ -0,0 +1,9 @@
+function myfunction(){
+            document.getElementById("mybtn").style.backgroundColor="pink";
+        }   
+function myalert(){
+    alert("Hello goodmorning")
+}
+function abcd(){
+    document. getElementById ("mypara").style.backgroundColor="red"
+} 
